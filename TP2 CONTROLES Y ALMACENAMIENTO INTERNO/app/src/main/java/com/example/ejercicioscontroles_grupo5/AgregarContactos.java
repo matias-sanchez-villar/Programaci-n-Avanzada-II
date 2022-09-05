c6c44de0e402f7ce18d32112492e3cfdea79809f
dadistefano <63522896+dadistefano@users.noreply.github.com>
2022-09-04 21:32:38 -0300

    Diseño Form Contacto+Spinner

--- TP2 CONTROLES Y ALMACENAMIENTO INTERNO/app/src/main/java/com/example/ejercicioscontroles_grupo5/AgregarContactos.java
+++ TP2 CONTROLES Y ALMACENAMIENTO INTERNO/app/src/main/java/com/example/ejercicioscontroles_grupo5/AgregarContactos.java
@@ -2,12 +2,37 @@ package com.example.ejercicioscontroles_grupo5;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 public class AgregarContactos extends AppCompatActivity {
+
+    Spinner spTelefono;
+    Spinner spEmail;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agregar_contactos);
+
+
+        spTelefono=findViewById(R.id.spTelefono);
+        spEmail=findViewById(R.id.spMail);
+
+        String [] opciones = {"Casa", "Trabajo", "Personal"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, opciones);
+        spTelefono.setAdapter(adapter);
+        spEmail.setAdapter(adapter);
+
+
     }
+
+
+
+
 }
