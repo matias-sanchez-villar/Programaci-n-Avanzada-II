1c9bdade6b21cee0f337fdcaf180fe6cd595477d
Elmer Vasquez <elmer.vasquez@mangoodev.com>
2022-09-06 13:01:13 -0300

    Cambios de Marcos

--- TP2 CONTROLES Y ALMACENAMIENTO INTERNO/app/src/main/java/com/example/ejercicioscontroles_grupo5/AgregarContactos.java
+++ TP2 CONTROLES Y ALMACENAMIENTO INTERNO/app/src/main/java/com/example/ejercicioscontroles_grupo5/AgregarContactos.java
@@ -1,7 +1,10 @@
 package com.example.ejercicioscontroles_grupo5;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -18,6 +21,7 @@ public class AgregarContactos extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
         setContentView(R.layout.activity_agregar_contactos);
 
 
@@ -32,7 +36,30 @@ public class AgregarContactos extends AppCompatActivity {
 
     }
 
+    @Override
+    public boolean onCreateOptionsMenu (Menu menu){
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
 
+    public void agregarActivity (View view){
+        Intent agregar = new Intent(this, AgregarContactos2.class);
+        startActivity(agregar);
+    }
+
+    public void listadoActivity (View view){
+        Intent listado = new Intent(this, ListadoContactos.class);
+        startActivity(listado);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected (MenuItem item){
+        int id = item.getItemId();
+        if (id == R.id.agregar) agregarActivity(null);
+        if (id == R.id.listar) listadoActivity(null);
+        if (id == R.id.listar || id == R.id.agregar) return true;
+        return super.onOptionsItemSelected(item);
+    }
 
 
 }
--- TP2 CONTROLES Y ALMACENAMIENTO INTERNO/app/src/main/java/com/example/ejercicioscontroles_grupo5/ListadoContactos.java
+++ TP2 CONTROLES Y ALMACENAMIENTO INTERNO/app/src/main/java/com/example/ejercicioscontroles_grupo5/ListadoContactos.java
@@ -1,7 +1,11 @@
 package com.example.ejercicioscontroles_grupo5;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 public class ListadoContactos extends AppCompatActivity {
 
@@ -9,5 +13,31 @@ public class ListadoContactos extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listado_contactos);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu (Menu menu){
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    public void agregarActivity (View view){
+        Intent agregar = new Intent(this, AgregarContactos.class);
+        startActivity(agregar);
+    }
+
+    public void listadoActivity (View view){
+        Intent listado = new Intent(this, ListadoContactos.class);
+        startActivity(listado);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected (MenuItem item){
+        int id = item.getItemId();
+        if (id == R.id.agregar) agregarActivity(null);
+        if (id == R.id.listar) listadoActivity(null);
+        if (id == R.id.listar || id == R.id.agregar) return true;
+        return super.onOptionsItemSelected(item);
     }
 }
