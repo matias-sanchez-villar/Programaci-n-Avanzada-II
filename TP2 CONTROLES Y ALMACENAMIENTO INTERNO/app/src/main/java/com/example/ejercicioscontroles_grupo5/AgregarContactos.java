d9d56e3bcdf7ebb1417cc62e426ddde2a46ce8d7
Elmer Vasquez <elmer.vasquez@mangoodev.com>
2022-09-06 21:05:31 -0300

    Agregar contacto

--- TP2 CONTROLES Y ALMACENAMIENTO INTERNO/app/src/main/java/com/example/ejercicioscontroles_grupo5/AgregarContactos.java
+++ TP2 CONTROLES Y ALMACENAMIENTO INTERNO/app/src/main/java/com/example/ejercicioscontroles_grupo5/AgregarContactos.java
@@ -8,6 +8,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 import java.util.ArrayList;
@@ -16,7 +17,12 @@ public class AgregarContactos extends AppCompatActivity {
 
     Spinner spTelefono;
     Spinner spEmail;
-
+    private EditText nombre;
+    private EditText apellido;
+    private EditText telefono;
+    private EditText email;
+    private EditText direccion;
+    private EditText fechaNacimiento;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +33,13 @@ public class AgregarContactos extends AppCompatActivity {
 
         spTelefono=findViewById(R.id.spTelefono);
         spEmail=findViewById(R.id.spMail);
+
+        nombre = (EditText) findViewById(R.id.editTextNombre);
+        apellido = (EditText) findViewById(R.id.editTextApellido);
+        telefono = (EditText) findViewById(R.id.editTextPhone);
+        email = (EditText) findViewById(R.id.editTextTextEmailAddress);
+        direccion = (EditText) findViewById(R.id.editTextDireccion);
+        fechaNacimiento = (EditText) findViewById(R.id.editTextDate);
 
         String [] opciones = {"Casa", "Trabajo", "Personal"};
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, opciones);
@@ -43,13 +56,26 @@ public class AgregarContactos extends AppCompatActivity {
     }
 
     public void agregarActivity (View view){
-        Intent agregar = new Intent(this, AgregarContactos2.class);
+        Intent agregar = new Intent(this, AgregarContactos.class);
         startActivity(agregar);
     }
 
     public void listadoActivity (View view){
         Intent listado = new Intent(this, ListadoContactos.class);
         startActivity(listado);
+    }
+
+    public void agregarActivity2 (View view){
+        Intent agregar2 = new Intent(this, AgregarContactos2.class);
+        agregar2.putExtra("nombre", nombre.getText().toString());
+        agregar2.putExtra("apellido", apellido.getText().toString());
+        agregar2.putExtra("telefono", telefono.getText().toString());
+        agregar2.putExtra("email", email.getText().toString());
+        agregar2.putExtra("direccion", direccion.getText().toString());
+        agregar2.putExtra("fechaNacimiento", fechaNacimiento.getText().toString());
+        agregar2.putExtra("spTelefono", spTelefono.getSelectedItem().toString());
+        agregar2.putExtra("spEmail", spEmail.getSelectedItem().toString());
+        startActivity(agregar2);
     }
 
     @Override
@@ -60,6 +86,8 @@ public class AgregarContactos extends AppCompatActivity {
         if (id == R.id.listar || id == R.id.agregar) return true;
         return super.onOptionsItemSelected(item);
     }
+
+
 
 
 }
--- TP2 CONTROLES Y ALMACENAMIENTO INTERNO/app/src/main/java/com/example/ejercicioscontroles_grupo5/AgregarContactos2.java
+++ TP2 CONTROLES Y ALMACENAMIENTO INTERNO/app/src/main/java/com/example/ejercicioscontroles_grupo5/AgregarContactos2.java
@@ -1,13 +1,79 @@
 package com.example.ejercicioscontroles_grupo5;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyboardShortcutGroup;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Switch;
+import android.widget.Toast;
 
 public class AgregarContactos2 extends AppCompatActivity {
+
+    private RadioButton primarioIncompleto;
+    private RadioButton primarioCompleto;
+    private RadioButton secundarioCompleto;
+    private RadioButton secundarioIncompleto;
+    private RadioButton otros;
+    private CheckBox deporte;
+    private CheckBox arte;
+    private CheckBox musica;
+    private CheckBox tecnologia;
+    private Switch recibeInformacion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agregar_contactos2);
+
+        primarioIncompleto = (RadioButton) findViewById(R.id.rbPrimarioIncompleto);
+        primarioCompleto = (RadioButton) findViewById(R.id.rbPrimarioCompleto);
+        secundarioCompleto = (RadioButton) findViewById(R.id.rbSecundarioCompleto);
+        secundarioIncompleto = (RadioButton) findViewById(R.id.rbSecundarioIncompleto);
+        otros = (RadioButton) findViewById(R.id.rbOtros);
+        deporte = (CheckBox) findViewById(R.id.cbDeporte);
+        arte = (CheckBox) findViewById(R.id.cbArte);
+        musica = (CheckBox) findViewById(R.id.cbMusica);
+        tecnologia = (CheckBox) findViewById(R.id.cbTecnologia);
+        recibeInformacion = (Switch) findViewById(R.id.recibeInformacion);
+    }
+
+    public void Guardar(View view){
+        String nombre = getIntent().getStringExtra("nombre");
+        String apellido = getIntent().getStringExtra("apellido");
+        String telefono = getIntent().getStringExtra("telefono");
+        String email = getIntent().getStringExtra("email");
+        String direccion = getIntent().getStringExtra("direccion");
+        String fechaNacimiento = getIntent().getStringExtra("fechaNacimiento");
+        String spTelefono = getIntent().getStringExtra("spTelefono");
+        String spEmail = getIntent().getStringExtra("spEmail");
+        SharedPreferences preferences = getSharedPreferences("contactos", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString("nombre", nombre);
+        editor.putString("apellido", apellido);
+        editor.putString("telefono", telefono);
+        editor.putString("email", email);
+        editor.putString("direccion", direccion);
+        editor.putString("fechaNacimiento", fechaNacimiento);
+        editor.putString("spTelefono", spTelefono);
+        editor.putString("spEmail", spEmail);
+        editor.putBoolean("pCompleto", primarioCompleto.isChecked());
+        editor.putBoolean("pIncompleto", primarioIncompleto.isChecked());
+        editor.putBoolean("sCompleto", secundarioCompleto.isChecked());
+        editor.putBoolean("sIncompleto", secundarioIncompleto.isChecked());
+        editor.putBoolean("otros", otros.isChecked());
+        editor.putBoolean("deporte", deporte.isChecked());
+        editor.putBoolean("arte", arte.isChecked());
+        editor.putBoolean("musica", musica.isChecked());
+        editor.putBoolean("tecnologica", tecnologia.isChecked());
+        editor.putBoolean("recibeInformacion", recibeInformacion.isChecked());
+
+        editor.commit();
+
+        Toast.makeText(this, "El contacto ha sido guardado", Toast.LENGTH_SHORT).show();
     }
 }
