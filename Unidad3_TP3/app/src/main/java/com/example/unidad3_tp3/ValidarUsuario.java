6084d619fead27702923dce9fd0335a4802bd8f2
Matias Sanchez Villar <67757088+matias-sanchez-villar@users.noreply.github.com>
2022-09-23 17:04:39 -0300

    validar usuario parametros final

--- Unidad3_TP3/app/src/main/java/com/example/unidad3_tp3/ValidarUsuario.java
+++ Unidad3_TP3/app/src/main/java/com/example/unidad3_tp3/ValidarUsuario.java
@@ -5,9 +5,9 @@ import java.util.regex.Pattern;
 
 public class ValidarUsuario {
 
-    public static String emailRegex = "^[_A-Za-z0-9-\\\\+]+(\\\\.[_A-Za-z0-9-]+)*@\"\n\"[A-Za-z0-9-]+(\\\\.[A-Za-z0-9]+)*(\\\\.[A-Za-z]{2,})$";
-    public static String nombreRegex = "^[A-Za-z]{1,20}$";
-    public static String passwordRegex = "";
+    public static final String emailRegex = "^[_A-Za-z0-9-\\\\+]+(\\\\.[_A-Za-z0-9-]+)*@\"\n\"[A-Za-z0-9-]+(\\\\.[A-Za-z0-9]+)*(\\\\.[A-Za-z]{2,})$";
+    public static final String nombreRegex = "^[A-Za-z]{1,20}$";
+    public static final String passwordRegex = "";
 
     public static boolean nombre (String nombre) {
         Pattern pattern = Pattern.compile(emailRegex);
