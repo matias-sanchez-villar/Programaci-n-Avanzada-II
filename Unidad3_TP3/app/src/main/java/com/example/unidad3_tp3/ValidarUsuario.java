c9ed30601cecec2ac2cf9d65434084ea4536f9cd
Matias Sanchez Villar <67757088+matias-sanchez-villar@users.noreply.github.com>
2022-09-23 16:08:54 -0300

    configuracion del activity

---
+++ Unidad3_TP3/app/src/main/java/com/example/unidad3_tp3/ValidarUsuario.java
@@ -0,0 +1,17 @@
+package com.example.unidad3_tp3;
+
+public class ValidarUsuario {
+
+    public static boolean nombre (String nombre) {
+        return  true;
+    }
+
+    public static boolean email (String email) {
+        return  true;
+    }
+
+    public static boolean password (String password){
+        return  true;
+    }
+
+}
