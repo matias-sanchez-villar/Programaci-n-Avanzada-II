aaf49862102f4318b0270652a141ebfa58417114
Matias Sanchez Villar <67757088+matias-sanchez-villar@users.noreply.github.com>
2022-10-20 09:42:54 -0300

    alta lista

--- Unidad4/app/src/main/java/com/example/unidad4/AltaFragment.java
+++ Unidad4/app/src/main/java/com/example/unidad4/AltaFragment.java
@@ -3,62 +3,73 @@ package com.example.unidad4;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AltaFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+
 public class AltaFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public AltaFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AltaFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AltaFragment newInstance(String param1, String param2) {
-        AltaFragment fragment = new AltaFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private EditText txtId, txtNombreProducto, txtStock;
+    private Spinner spnCategorias;
+    private Button btnAgregar;
+    View view;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
+
+    public AltaFragment() {}
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_alta, container, false);
+        view = inflater.inflate(R.layout.fragment_alta, container, false);
+        fillProperties();
+        btnAgregar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onClickBtnAgregar();
+            }
+        });
+        return view;
+    }
+
+    public void fillProperties(){
+        txtId = (EditText)view.findViewById(R.id.txtId);
+        txtNombreProducto = (EditText)view.findViewById(R.id.txtNombreProducto);
+        txtStock = (EditText)view.findViewById(R.id.txtStock);
+        spnCategorias = (Spinner) view.findViewById(R.id.spnCategorias);
+        btnAgregar = (Button) view.findViewById(R.id.btnAgregar);
+
+        //No
+        String [] nombres = {"pepe", "jose", "carolina", "luicio"};
+        //Si
+        ArrayAdapter<String> categorias = new ArrayAdapter<String>(
+                getActivity(),
+                android.R.layout.simple_spinner_item,
+                nombres
+        );
+        spnCategorias.setAdapter(categorias);
+        spnCategorias.setPrompt("Categorias");
+    }
+
+    public void onClickBtnAgregar(){
+        String id = txtId.getText().toString();
+
+        //seteamos
+        txtNombreProducto.setText("pepe");
+        txtStock.setText("22");
+
     }
 }
