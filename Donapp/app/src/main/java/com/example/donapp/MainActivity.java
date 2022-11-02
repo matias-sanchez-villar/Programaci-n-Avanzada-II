2617641d6221393f371353e8337afb68fab4846d
matiassa <matuite96@gmail.com>
2022-11-01 23:33:24 -0300

    Entity localidad, provincia y solicitud

--- Donapp/app/src/main/java/com/example/donapp/MainActivity.java
+++ Donapp/app/src/main/java/com/example/donapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.donapp;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 public class MainActivity extends AppCompatActivity {
 
