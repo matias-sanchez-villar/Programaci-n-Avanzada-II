f7e0d176eb0269ea8854ec4852ef97a4bacc1c0f
matiassa <67757088+matias-sanchez-villar@users.noreply.github.com>
2022-11-09 19:53:48 -0300

    creado

---
+++ Donapp/app/src/main/java/com/example/donapp/Activity/AltaHistorialMedico.java
@@ -0,0 +1,14 @@
+package com.example.donapp.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AltaHistorialMedico extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alta_historial_medico);
+    }
+}
