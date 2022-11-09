1860c9cb7026aad16388f8e4d0c4f01719bd1ddd
matiassa <67757088+matias-sanchez-villar@users.noreply.github.com>
2022-11-08 21:49:37 -0300

    Read Solicitud

--- Donapp/app/src/main/java/com/example/donapp/Data/Solicitud/ReadSolicitudAsync.java
+++ Donapp/app/src/main/java/com/example/donapp/Data/Solicitud/ReadSolicitudAsync.java
@@ -6,8 +6,16 @@ import android.os.AsyncTask;
 import com.example.donapp.Entity.Solicitud;
 
 public class ReadSolicitudAsync extends AsyncTask<String, Void, Solicitud> {
+
     Solicitud solicitud;
     private Context context;
+    private int searcheableId;
+
+    public ReadSolicitudAsync(int id, Context context){
+        this.searcheableId = id;
+        this.context = context;
+    }
+
 
     @Override
     protected Solicitud doInBackground(String... strings) {
