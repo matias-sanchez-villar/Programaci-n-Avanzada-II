4c2c25c06472d714d2509ead6550a965d9f1b84a
matiassa <67757088+matias-sanchez-villar@users.noreply.github.com>
2022-11-08 23:16:53 -0300

    Spinner en progreso

--- Donapp/app/src/main/java/com/example/donapp/Activity/AltaSolicitud.java
+++ Donapp/app/src/main/java/com/example/donapp/Activity/AltaSolicitud.java
@@ -4,55 +4,99 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.example.donapp.Data.Localidad.LocalidadRepository;
+import com.example.donapp.Data.Provincia.ProvinciaRepository;
+import com.example.donapp.Data.Solicitud.SolicitudRepository;
 import com.example.donapp.Entity.Localidad;
 import com.example.donapp.Entity.Provincia;
 import com.example.donapp.Entity.Solicitud;
 import com.example.donapp.R;
-import com.example.donapp.Util.DateUtil;
 import com.example.donapp.Util.Validar;
-
-import java.util.ArrayList;
 
 public class AltaSolicitud extends AppCompatActivity {
 
     private EditText txtNombre, txtApellido, txtFecha, txtDireccion, txtCantDonante;
-    private Spinner spnLocalidad, spnProvincia,spnTipoSangre;
-    private Button btnCerrarSession, btnCerrar, btnGuardar;
+    private Spinner spnLocalidad, spnProvincia, spnTipoSangre;
+    private Button btnGuardar;
+    private ProvinciaRepository _provinciaRepository;
+    private LocalidadRepository _localidadRepository;
+    private Provincia provinciaSelected;
+    private Localidad localidadSelected;
+    private SolicitudRepository solicitudRepository;
+    private Solicitud solicitud;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alta_solicitud);
         fillProperties();
+        setListeners();
+        getDBInfo();
     }
 
-    public void onClickBtnGuardar(){
+    public void onClickBtnGuardar(View view){
         if (validateData())
             return;
-
-        //TODO: Solo queda guardar en la BD
 
     }
 
     public void fillProperties(){
-        //TODO queda traer los datos para los SPINNER
+        solicitudRepository = new SolicitudRepository(this);
+        _provinciaRepository = new ProvinciaRepository(this);
+        _localidadRepository = new LocalidadRepository(this);
 
-        txtNombre = (EditText)this.findViewById(R.id.txtNombre);
-        txtApellido = (EditText)this.findViewById(R.id.txtApellido);
-        txtFecha = (EditText)this.findViewById(R.id.txtFecha);
-        txtDireccion = (EditText)this.findViewById(R.id.txtDireccion);
-        txtCantDonante = (EditText)this.findViewById(R.id.txtCantDonante);
-        spnLocalidad = (Spinner)this.findViewById(R.id.spnLocalidad);
-        spnProvincia = (Spinner)this.findViewById(R.id.spnProvincia);
-        spnTipoSangre = (Spinner)this.findViewById(R.id.spnTipoSangre);
-        btnCerrarSession = (Button)this.findViewById(R.id.btnCerrarSession);
-        btnCerrar = (Button)this.findViewById(R.id.btnCerrar);
-        btnGuardar = (Button)this.findViewById(R.id.btnGuardar);
+        txtNombre = (EditText)findViewById(R.id.txtNombre);
+        txtApellido = (EditText)findViewById(R.id.txtApellido);
+        txtFecha = (EditText)findViewById(R.id.txtFecha);
+        txtDireccion = (EditText)findViewById(R.id.txtDireccion);
+        txtCantDonante = (EditText)findViewById(R.id.txtCantDonante);
+        spnLocalidad = (Spinner)findViewById(R.id.spnLocalidad);
+        spnProvincia = (Spinner)findViewById(R.id.spnProvincia);
+        spnTipoSangre = (Spinner)findViewById(R.id.spnTipoSangre);
+        btnGuardar = (Button)findViewById(R.id.btnGuardar);
+    }
+
+    public void setListeners() {
+        spnProvincia.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                provinciaSelected = (Provincia) parent.getSelectedItem();
+                // Seteamos a 0 los items que pueda tener;
+                spnLocalidad.setAdapter(null);
+                getLocalidadByProvincia(provinciaSelected.getId());
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        spnLocalidad.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                localidadSelected = (Localidad) parent.getSelectedItem();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
+
+    public void getDBInfo(){
+        _provinciaRepository.selectAllForSpinner(spnProvincia);
+    }
+
+    public void getLocalidadByProvincia(int provinciaId){
+        _localidadRepository.selectAllForSpinnerByProvincia(spnLocalidad, provinciaId);
     }
 
     public boolean validateData(){
