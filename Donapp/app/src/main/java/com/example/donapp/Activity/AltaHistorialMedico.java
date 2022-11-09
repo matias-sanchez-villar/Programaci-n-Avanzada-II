ff058da952c204acd5fef658227c4a0a0261bec6
matiassa <67757088+matias-sanchez-villar@users.noreply.github.com>
2022-11-09 20:38:35 -0300

    Vista lista

--- Donapp/app/src/main/java/com/example/donapp/Activity/AltaHistorialMedico.java
+++ Donapp/app/src/main/java/com/example/donapp/Activity/AltaHistorialMedico.java
@@ -3,6 +3,9 @@ package com.example.donapp.Activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.donapp.R;
 
 public class AltaHistorialMedico extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class AltaHistorialMedico extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alta_historial_medico);
+    }
+
+
+
+    public void onClickBtnGuardar(View view){
+
     }
 }
