0e32abbe152530b3929841ff69deed48bf283aea
matiassa <67757088+matias-sanchez-villar@users.noreply.github.com>
2022-11-10 13:16:19 -0300

    creado

---
+++ Donapp/app/src/main/java/com/example/donapp/Activity/DetalleBancoSangre.java
@@ -0,0 +1,14 @@
+package com.example.donapp.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetalleBancoSangre extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalle_banco_sangre);
+    }
+}
