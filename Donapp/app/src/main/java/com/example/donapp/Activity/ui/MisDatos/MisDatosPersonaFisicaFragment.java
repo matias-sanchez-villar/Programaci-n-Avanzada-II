704ae69497798f3b20e0fde5d05e3293498e1589
matiassa <67757088+matias-sanchez-villar@users.noreply.github.com>
2022-11-10 21:36:28 -0300

    fillProperties

--- Donapp/app/src/main/java/com/example/donapp/Activity/ui/MisDatos/MisDatosPersonaFisicaFragment.java
+++ Donapp/app/src/main/java/com/example/donapp/Activity/ui/MisDatos/MisDatosPersonaFisicaFragment.java
@@ -4,16 +4,28 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.fragment.app.Fragment;
 
+import com.example.donapp.Data.Campania.CampaniaRepository;
+import com.example.donapp.Data.Localidad.LocalidadRepository;
+import com.example.donapp.Data.Provincia.ProvinciaRepository;
+import com.example.donapp.R;
 import com.example.donapp.databinding.FragmentMisDatosPersonaFisicaBinding;
 
 public class MisDatosPersonaFisicaFragment extends Fragment{
 
-    private FragmentMisDatosPersonaFisicaBinding binding;
+    FragmentMisDatosPersonaFisicaBinding binding;
+    TextView txtNombre, txtTelefono, txtDireccion, txtProvincia, txtEmail,
+            txtLocalidad, txtApellido, txtDNI, txtFechaNacimiento, txtContasena;
+    Button btnModifica;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -24,6 +36,20 @@ public class MisDatosPersonaFisicaFragment extends Fragment{
         View root = binding.getRoot();
 
         return root;
+    }
+
+    private void fillProperties() {
+        txtNombre = (EditText)binding.txtNombreResponseMDF;
+        txtApellido = (EditText)binding.txtApellidoResponseMDF;
+        txtFechaNacimiento = (EditText)binding.txtFechaNacimientoResponseMDF;
+        txtDNI = (EditText)binding.txtDNIResponseMDF;
+        txtTelefono = (EditText)binding.txtTelefonoResponseMDF;
+        txtProvincia = (EditText)binding.txtProvinciaResponseMDF;
+        txtLocalidad = (EditText)binding.txtLocalidadResponseMDF;
+        txtDireccion = (EditText)binding.txtDireccionResponseMDF;
+        txtEmail = (EditText)binding.txtEmailResponseMDF;
+        txtContasena = (EditText)binding.txtContrasenaResponseDPF;
+        btnModifica = (Button)binding.btnMoficiarDPF;
     }
 
     @Override
--- Donapp/app/src/main/java/com/example/donapp/Activity/ui/MisDatos/MisDatosPersonaJuridicaFragment.java
+++ Donapp/app/src/main/java/com/example/donapp/Activity/ui/MisDatos/MisDatosPersonaJuridicaFragment.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.ViewModelProvider;
@@ -14,6 +17,9 @@ import com.example.donapp.databinding.FragmentMisDatosPersonaJuridicaBinding;
 public class MisDatosPersonaJuridicaFragment extends Fragment{
 
     private FragmentMisDatosPersonaJuridicaBinding binding;
+    TextView txtNombre, txtTelefono, txtDireccion, txtProvincia, txtLocalidad, txtEmail,
+            txtCUIL, txtHoraInicio, txtHoraFin, txtContasena;
+    Button btnModifica;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -24,6 +30,20 @@ public class MisDatosPersonaJuridicaFragment extends Fragment{
         View root = binding.getRoot();
 
         return root;
+    }
+
+    private void fillProperties() {
+        txtNombre = (EditText)binding.txtNombreResponseMDJ;
+        txtCUIL = (EditText)binding.txtCuilResponseMDJ;
+        txtHoraInicio = (EditText)binding.txtHoraInicioResponseMDJ;
+        txtHoraFin = (EditText)binding.txtHoraCierreResponseMDJ;
+        txtTelefono = (EditText)binding.txtTelefonoResponseMDJ;
+        txtProvincia = (EditText)binding.txtProvinciaResponseMDJ;
+        txtLocalidad = (EditText)binding.txtLocalidadResponseMDJ;
+        txtDireccion = (EditText)binding.txtDireccionResponseMDJ;
+        txtEmail = (EditText)binding.txtEmailResponseMDJ;
+        txtContasena = (EditText)binding.txtContrasenaResponseDPJ;
+        btnModifica = (Button)binding.btnMoficiarDPJ;
     }
 
     @Override
