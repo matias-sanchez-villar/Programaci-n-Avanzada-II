bbe96c77734b462bf8cae644daa050297f67a4f5
Elmer Vasquez <elmer.vasquez@mangoodev.com>
2022-11-11 18:53:29 -0300

    SelectEntityList

---
+++ Donapp/app/src/main/java/com/example/donapp/Data/InstitucionesMedicas/DataInstitucionMedicaAsync.java
@@ -0,0 +1,2 @@
+package com.example.donapp.Data.InstitucionesMedicas;public class DataInstitucionMedicaAsync {
+}
---
+++ Donapp/app/src/main/java/com/example/donapp/Data/InstitucionesMedicas/InstitucionMedicaRepository.java
@@ -0,0 +1,4 @@
+package com.example.donapp.Data.InstitucionMedica;
+
+public class InstitucionMedicaRepository {
+}
