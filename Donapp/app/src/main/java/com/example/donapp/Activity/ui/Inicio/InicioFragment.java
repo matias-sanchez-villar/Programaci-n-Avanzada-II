1f02f657905a2a65c87cb9b7f78f3d5fd3f3173c
marcoos9913 <79883722+Marcos215@users.noreply.github.com>
2022-11-11 13:45:24 -0300

    Cambios Inicio

--- Donapp/app/src/main/java/com/example/donapp/Activity/ui/Inicio/InicioFragment.java
+++ Donapp/app/src/main/java/com/example/donapp/Activity/ui/Inicio/InicioFragment.java
@@ -1,9 +1,12 @@
 package com.example.donapp.Activity.ui.Inicio;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -15,6 +18,7 @@ import com.example.donapp.databinding.FragmentInicioBinding;
 public class InicioFragment extends Fragment {
 
     private FragmentInicioBinding binding;
+    private Button btnVerMasInformacion;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -24,14 +28,28 @@ public class InicioFragment extends Fragment {
         binding = FragmentInicioBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        VerMasInformacion();
+
         return root;
     }
 
+    public void VerMasInformacion(){
+        btnVerMasInformacion =(Button) binding.btnVerMasInfo;
+        btnVerMasInformacion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.argentina.gob.ar/salud/donarsangre/faq"));
+                startActivity(i);
+            }
+        });
+    }
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
     }
+
+
+
+
 }
