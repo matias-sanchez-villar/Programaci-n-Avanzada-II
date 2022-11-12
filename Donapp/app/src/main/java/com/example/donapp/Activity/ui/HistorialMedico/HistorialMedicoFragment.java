2e2a5ebd27cf11838d86f3373f00132884405bd6
matiassa <67757088+matias-sanchez-villar@users.noreply.github.com>
2022-11-12 02:12:04 -0300

    historial medico

--- Donapp/app/src/main/java/com/example/donapp/Activity/ui/HistorialMedico/HistorialMedicoFragment.java
+++ Donapp/app/src/main/java/com/example/donapp/Activity/ui/HistorialMedico/HistorialMedicoFragment.java
@@ -19,6 +19,7 @@ import com.example.donapp.Data.Postulacion.PostulacionRepository;
 import com.example.donapp.Entity.GlobalPreferences;
 import com.example.donapp.Entity.HistorialMedico;
 import com.example.donapp.Entity.Usuario;
+import com.example.donapp.Enums.TipoSangre;
 import com.example.donapp.databinding.FragmentHistorialMedicoBinding;
 
 public class HistorialMedicoFragment extends Fragment{
@@ -31,6 +32,10 @@ public class HistorialMedicoFragment extends Fragment{
     HistorialMedicoRepository _historialMedicoRepository;
     PostulacionRepository _postulacionRepository;
     HistorialMedico historialMedico;
+    TextView tipoSangre, peso, altura, fechaDonacion, tatuajes, vacunas,
+            examenSangre, revisionMedica, tratamientoDental, endoscopia,
+            embaraso, enfermedad, operacion, viaje, anemia, accidentes,
+            usoMedicamentes, hepatitis;
 
 
     private FragmentHistorialMedicoBinding binding;
@@ -64,6 +69,25 @@ public class HistorialMedicoFragment extends Fragment{
         sinPostulacionesTxt = binding.titlePostulacionesFragmentHistorialMedico;
         fragmentTittle = binding.titleFragmentHistorialMedico;
         historialMainScrollView = binding.scrollFragmentHistorialMedico;
+
+        tipoSangre = binding.tipoSangreDHM;
+        peso = binding.pesoDHM;
+        altura = binding.alturaDHM;
+        fechaDonacion = binding.ultimaDonacionDHM;
+        tatuajes = binding.tatuajeDHM;
+        vacunas = binding.vacunaDHM;
+        examenSangre = binding.examenDHM;
+        revisionMedica = binding.revisionDHM;
+        tratamientoDental = binding.tratamientoDHM;
+        endoscopia = binding.endoscopiaDHM;
+        embaraso = binding.embarazoDHM;
+        enfermedad = binding.enfermedadDHM;
+        operacion = binding.operacionDHM;
+        viaje = binding.viajeDHM;
+        anemia = binding.anemiaDHM;
+        accidentes = binding.accidenteDHM;
+        usoMedicamentes = binding.medicamentosDHM;
+        hepatitis = binding.hepatitisDHM;
     }
 
     public void setListeners(){
@@ -96,13 +120,36 @@ public class HistorialMedicoFragment extends Fragment{
             fragmentTittle.setText("Historial Médico");
             btnAltaHistorialMedico.setText("EDITAR HISTORIAL MÉDICO");
             sinPostulacionesTxt.setText("Historial Postulaciones");
-        } else {
-         historialMainScrollView.setVisibility(View.GONE);
+            setProperties(historialMedico);
         }
+        else historialMainScrollView.setVisibility(View.GONE);
+
 
         _postulacionRepository.selectAllById(
                 lvHistorialPostulaciones,
                 GlobalPreferences.getLoggedUserId(getActivity())
         );
     }
+
+    public void setProperties(HistorialMedico historialMedico){
+        tipoSangre.setText(TipoSangre.getStringByPosition(historialMedico.getTipoSangre().ordinal()));
+        peso.setText(String.valueOf(historialMedico.getPeso()));
+        altura.setText(historialMedico.getAltura().toString());
+        fechaDonacion.setText(historialMedico.getUltimaDonacion().toString());
+        tatuajes.setText(historialMedico.isTatuajes() == true ? "si" : "no");
+        vacunas.setText(historialMedico.isVacunaAlergia() == true ? "si" : "no");
+        examenSangre.setText(historialMedico.isExamenSangre() == true ? "si" : "no");
+        revisionMedica.setText(historialMedico.isRevisionMedica() == true ? "si" : "no");
+        tratamientoDental.setText(historialMedico.isTratamientoDental() == true ? "si" : "no");
+        endoscopia.setText(historialMedico.isEndoscopia() == true ? "si" : "no");
+        embaraso.setText(historialMedico.isEmbarazo() == true ? "si" : "no");
+        enfermedad.setText(historialMedico.isEnfermedaCronica() == true ? "si" : "no");
+        operacion.setText(historialMedico.isOperacion() == true ? "si" : "no");
+        viaje.setText(historialMedico.isViaje() == true ? "si" : "no");
+        anemia.setText(historialMedico.isAnemia() == true ? "si" : "no");
+        accidentes.setText(historialMedico.isAccidenteVascular() == true ? "si" : "no");
+        usoMedicamentes.setText(historialMedico.isUsaMedicamentos() == true ? "si" : "no");
+        hepatitis.setText(historialMedico.isHepatitis() == true ? "si" : "no");
+    }
+
 }
